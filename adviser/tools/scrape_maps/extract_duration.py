f0ad0db539c5a2f91810aacfd894208a5dd26562
ycyang18 <77813308+ycyang18@users.noreply.github.com>
2022-06-11 22:02:41 +0200

    Create extract_duration.py

---
+++ adviser/tools/scrape_maps/extract_duration.py
@@ -0,0 +1,26 @@
+import requests
+import folium
+import polyline
+
+def get_route(start_lon, start_lat, destination_lon, destination_lat):
+    
+    loc = "{},{};{},{}".format(start_lon, start_lat, destination_lon, destination_lat)
+    url = "http://router.project-osrm.org/route/v1/bike/"
+    r = requests.get(url + loc) 
+    if r.status_code!= 200:
+        return {}
+    res = r.json()   
+    #routes = polyline.decode(res['routes'][0]['geometry'])
+    start_point = [res['waypoints'][0]['location'][1], res['waypoints'][0]['location'][0]]
+    end_point = [res['waypoints'][1]['location'][1], res['waypoints'][1]['location'][0]]
+    distance = res['routes'][0]['distance']
+    duration = res['routes'][0]['duration']
+
+    out = {
+        'start_point': start_point,
+        'end_point': end_point,
+        'distance': distance,
+        'duration': "%d:%02d"%(duration//3600, duration//60)
+         }
+
+    return out['duration']
