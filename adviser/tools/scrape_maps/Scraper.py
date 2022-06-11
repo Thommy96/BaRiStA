93faec1a965eb0cf7a69b9bb39fd687301815b59
ycyang18 <77813308+ycyang18@users.noreply.github.com>
2022-06-11 22:21:12 +0200

    Update Scraper.py

--- adviser/tools/scrape_maps/Scraper.py
+++ adviser/tools/scrape_maps/Scraper.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
+from extract_duration import get_route
 import time
 import random
 
