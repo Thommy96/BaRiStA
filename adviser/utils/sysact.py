793dcce422cb6e5b89e82008d0566ed0fe8f950c
Thomas <50203775+Thommy96@users.noreply.github.com>
2022-08-11 13:12:59 +0200

    Revert "add TellOpeningDay"
    This reverts commit 3406c14ac57cd69304020ad99d44a31cab06f1e2.

--- adviser/utils/sysact.py
+++ adviser/utils/sysact.py
@@ -42,7 +42,6 @@ class SysActionType(Enum):
     ConfirmGiveRating = 'confirm_giverating'
     AskWriteReview = 'ask_writereview'
     ConfirmWriteReview = 'confirm_writereview'
-    TellOpeningDay = 'tell_openingday'
 
 
 class SysAct(object):
